795eb9a2169b79a1389e56f9737b040c447c80e3
zyr3x <izzzum@gmail.com>
2013-12-11 23:38:09 +0300

    StorageVolume hack

--- src/com/android/settings/deviceinfo/Memory.java
+++ src/com/android/settings/deviceinfo/Memory.java
@@ -94,13 +94,13 @@ public class Memory extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.device_info_memory);
 
-        addCategory(StorageVolumePreferenceCategory.buildForInternal(context));
+        //addCategory(StorageVolumePreferenceCategory.buildForInternal(context));
 
         final StorageVolume[] storageVolumes = mStorageManager.getVolumeList();
         for (StorageVolume volume : storageVolumes) {
-            if (!volume.isEmulated()) {
+            //if (!volume.isEmulated()) {
                 addCategory(StorageVolumePreferenceCategory.buildForPhysical(context, volume));
-            }
+            //}
         }
 
         setHasOptionsMenu(true);
